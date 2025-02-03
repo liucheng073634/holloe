4664ec9b2f7c7e440c03a654d767758969bc29c7
Your Name <3268144062@qq.com>
2025-02-03 20:10:55 +0800

    项目初始化

---
+++ gulimall-commo/src/test/java/com/atguigu/common/test/GulimallCommoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.atguigu.common.test;
+
+import com.atguigu.common.GulimallCommoApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCommoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("hello");
+    }
+
+}
